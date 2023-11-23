5246f7846e4b08c417034f51e628f67bf5a1b37d
Satyendra Sharma <146451948+Satyendra2006@users.noreply.github.com>
2023-11-23 17:54:28 +0530

    Add files via upload

---
+++ CSPracticalExp7.c/CSPracticalExp7Part2.c
@@ -0,0 +1,59 @@
+// #include <stdio.h>
+// void main()
+// {
+//     int mat1[3][3],mat2[3][3],mat3[3][3],i,j,k,mult;
+//     for(i=0;i<3;i++)
+//     {
+//         for(j=0;j<3;j++)
+//         {
+//             printf("Enter element of matrix 1: ");
+//             printf("[%d][%d] = ",i,j);
+//             scanf("%d",&mat1[i][j]);
+//         }
+//     }
+//     for(i=0;i<3;i++)
+//     {
+//         for(j=0;j<3;j++)
+//         {
+//             printf("Enter element of matrix 2: ");
+//             printf("[%d][%d] = ",i,j);
+//             scanf("%d",&mat2[i][j]);
+//         }
+//     }
+//     for(i = 0;i<3;i++)
+//     {
+//         for(j=0;j<3;j++)
+//         {
+//             mat3[i][j] = 0;
+//             for(k=0;k<3;k++)
+//             {
+//             mat3[i][j]= mat3[i][j] + mat1[i][k] * mat2[k][j];
+//             }
+            
+//         }
+//     }    
+//     for(i = 0;i<3;i++)
+//     {
+//         for(j=0;j<3;j++)
+//         {
+//             printf("%d ",mat3[i][j]);
+//         }
+//         printf("\n");
+//     }
+// } 
+
+
+#include<stdio.h>
+void main()
+{
+ int num;
+ printf("enter the number");
+ scanf("%d",&num;
+ if(num<0)
+ {
+    printf("please enter positive number");
+    return 1;
+ }
+ num1=num;
+ int
+}
